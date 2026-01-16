5b09248005759f40f7c8c392eb40b154f3b79cb2
synasapmob <sonn51280@gmail.com>
2026-01-16 20:45:15 +0700

    remove files

--- fix-html.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('index.html', 'utf8');
-// Remove import map
-content = content.replace(/<script type="importmap">[\s\S]*?<\/script>/g, '');
-fs.writeFileSync('index.html', content);
-console.log('Import map removed from index.html');
